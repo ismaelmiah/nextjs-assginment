7c847d2bb395e907b9146e293b534352f957ffb7
Ismael Miah <ismail27.dec@gmail.com>
2023-12-06 12:00:00 +0600

    loading applied on read data

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children }: any) {
   return (
     <div>
-        <h2 className="bg-gray-800 text-white h-16 w-full text-lg text-center">Next Js Assignments</h2>
+        <h2 className="bg-gray-800 text-white h-8 w-full text-lg text-center">Next Js Assignments</h2>
         <main className="">{children}</main>
     </div>
   );
